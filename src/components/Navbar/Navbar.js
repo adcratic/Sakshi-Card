3b850044b55b9af08187fedf16603d2707ce7eb7
Harsh-Manoor <harshmanoor@gmail.com>
2021-09-25 18:34:15 +0530

    footer changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,6 @@ import { NavbarData } from './NavbarData';
 
 function Navbar() {
   const [click, setClick] = useState(false);
-
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
