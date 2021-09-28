4ee2c6a15090fdd073ebd006025503757366fcfe
Harsh Manoor <54639267+Harsh-Manoor@users.noreply.github.com>
2021-09-28 23:55:53 +0530

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -10,7 +10,7 @@ function HeroSection() {
       <div className="hero-container">
           <div className="hero-heading">
               {/* <div className="hero-subtitle">PREMIUM DESIGNS</div> */}
-              <h1>Imaging and Creation</h1>
+              <h1>Imaging-Creating-Printing</h1>
               <p>A single platform to save legal teams loads of time. Create serve requests across all 50 states and follow progress with live updates, keeping track of everything along the way.</p>
           </div>
           <div className="hero-img">
